1a05889927af30e8d74203f3a61e52ca4006e905
Jurgis Samaitis <aukslius@gmail.com>
2017-04-11 19:52:51 +0300

    Jurgis Inc. Cleanup Services
    Added a cleanup function to do what I planned:
    * removes punctuation,
    * symbols,
    * numbers,
    * makes everything lowercase
    * makes it a sexy dataframe
    
    Also some of the texts were missing, for example wsj (it requires
    subscription to view them) - I need more data to figure out each case.
    
    Next step will be to omit these links from search since they just waste
    time and memory.
    
    Also made it save into a single big file. The size of this file MIGHT
    cause issues but that is yet to be decided.

--- Web Scraper.py
+++ Web Scraper.py
@@ -13,7 +13,10 @@ def link_gen(results_path):
     return(links)
 
 def get_texts(urls):
-    texts = {'url': [], 'text': []}
+    texts = {
+        'url': [],
+        'text': []
+    }
     for url in urls:
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
@@ -26,9 +29,45 @@ def get_texts(urls):
         texts['text'].append(text)
     return(texts)
 
+def cleanup(texts):
+    clean_texts = {
+        'url' : texts['url'],
+        'text' : []
+    }
+    for text in texts['text']:
+        text = text.split()
+        temp_text = []
+        for word in text:
+            temp_text.append(''.join(e for e in word if e.isalnum() and not e.isdigit()))
+        temp_text = [t for t in temp_text if t]
+        text = ' '.join(temp_text)
+        text = text.lower()
+        clean_texts['text'].append(text)
+    return(pd.DataFrame.from_dict(clean_texts))
+
+def text_missing(texts):
+    missing = []
+    for i in range(len(texts.text)):
+        if '' == texts.text[i]:
+            missing.append(texts.url[i])
+    return(missing)
+
 links = link_gen('C:\\Users\\Jurgis\\Desktop\\Automatic-Article-Searcer-V2\\Data\\Results')
 #Make this interactive and/or automatically search for files
 
 texts = get_texts(links)
+texts = cleanup(texts)
+text_missing(texts)
+#This could be included inside cleanup function, but I need to figure out the causes for this issue.
+#For example wsj needs you to subscribe to view their articles.
+for missing_value in text_missing(texts):
+    texts = texts[texts.url != missing_value]
 
-
+dir_path = os.getcwd()
+try:
+    old_texts = pd.read_csv(dir_path + '\\Articles.csv', encoding = 'ISO-8859-1')
+except:
+    texts.to_csv(dir_path + '\\Articles.csv', index=False, index_label=False)
+else:
+    texts = pd.concat([texts,old_texts], ignore_index = True, axis = 0)
+    texts.to_csv(dir_path + '\\Articles.csv', index=False, index_label=False)
