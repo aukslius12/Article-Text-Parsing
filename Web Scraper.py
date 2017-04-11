97960ef0dd5c520c783d6f26bd27246d27dbade1
Jurgis Samaitis <aukslius@gmail.com>
2017-04-11 15:56:16 +0300

    Small update
    Moved redirected_urls to AAS-V2

--- Web Scraper.py
+++ Web Scraper.py
@@ -12,28 +12,6 @@ def link_gen(results_path):
             links.append(link)
     return(links)
 
-def redirected_urls(urls):
-    rurls = []
-    failed_urls = []
-    for url in urls:
-        try:
-            page = requests.get(url).content
-        except:
-            failed_urls.append(url)
-        else:
-            soup = BeautifulSoup(page, 'lxml')
-            element = soup.find('meta', attrs={'http-equiv': 'refresh'})
-            try:
-                refresh_content = element['content']
-            except:
-                rurls.append(url)
-            else:
-                rurls.append(refresh_content.partition('=')[2][1:-1])
-    if len(failed_urls) == 0:
-        return({'Redirected Urls' : rurls})
-    else:
-        return({'Redirected Urls' : rurls, 'Failed Urls' : failed_urls})
-
 def get_texts(urls):
     texts = {'url': [], 'text': []}
     for url in urls:
@@ -48,13 +26,9 @@ def get_texts(urls):
         texts['text'].append(text)
     return(texts)
 
-raw_links = link_gen('C:\\Users\\Jurgis\\Desktop\\Automatic-Article-Searcer-V2\\Data\\Results')
+links = link_gen('C:\\Users\\Jurgis\\Desktop\\Automatic-Article-Searcer-V2\\Data\\Results')
 #Make this interactive and/or automatically search for files
 
-links = redirected_urls(raw_links)
-#print(links['Failed Urls'])
-#Failed urls should be relevant. Maybe if it exceeds some sort of treshold it should print a warning?
-
-texts = get_texts(links['Redirected Urls'])
+texts = get_texts(links)
 
 
